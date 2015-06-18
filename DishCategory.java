6b61de1a1e2ad0bfafa876c03367dc49c159bc6d
Andrii Vyhidnyi <wygidnyj@gmail.com>
2015-06-18 15:14:42 +0300

    add two clases

---
+++ DishCategory.java
@@ -0,0 +1,11 @@
+package complex;
+
+/** complex.DishCategory is an Enum contains category of
+ * dish existing in complex
+ *
+ * @author Andrii V <wygidnyj@gmail.com>
+ * @since 1.0
+ */
+public enum DishCategory {
+    FIRST, SECOND, DRINK
+}
---
+++ JsonHelper.java
@@ -0,0 +1,4 @@
+package complex;
+
+public class JsonHelper {
+}
